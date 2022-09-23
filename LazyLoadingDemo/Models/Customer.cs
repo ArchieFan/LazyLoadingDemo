6779e9c82a609ac2a359923d74917c375399a9c3
Archie Fan <archiefan@gmail.com>
2022-09-23 17:59:32 +0100

    Add project files.

---
+++ LazyLoadingDemo/Models/Customer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazyLoadingDemo.Models
+{
+    public class Customer
+    {
+        private Lazy<List<Order>> _orders = null;
+
+        public List<Order> Orders
+        {
+            get {
+                // Add below for lazy loading
+                //if (_orders == null)
+                //{
+                //    _orders = new List<Order>();
+                //}
+                return _orders.Value; 
+            }
+        }
+
+        private string _customerName;
+
+        public string CustomerName
+        {
+            get { return _customerName; }
+            set { _customerName = value; }
+        }
+
+        public Customer()
+        {
+            // Makes a database trip
+            _customerName = "Archie Fan";
+            // remove for lazy loading
+            //_orders = LoadOrders();
+            _orders = new Lazy<List<Order>>(() => LoadOrders());
+        }
+
+        private List<Order> LoadOrders()
+        {
+            List<Order> temp = new List<Order>();
+            Order o = new Order();
+            o.OrderNumber = "ord1001";
+            temp.Add(o);
+            o = new Order();
+            o.OrderNumber = "ord1002";
+            temp.Add(o);
+            return temp;
+        }
+    }
+}
---
+++ LazyLoadingDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using LazyLoadingDemo.Models;
+
+namespace LazyLoading
+{
+    class Program
+    {
+        static void Main()
+        {
+            // customer + Order will be loaded
+            Customer c = new Customer();    
+            Console.WriteLine(c.CustomerName);
+            foreach (var oitem in c.Orders)
+            {
+                Console.WriteLine(oitem.OrderNumber);
+            }
+        }
+    }
+
+
+}
